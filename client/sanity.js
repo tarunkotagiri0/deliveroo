017a0d26d59811f4255b868b588628cd03f9b19a
tarunkotagiri0 <tarun@migration.metalok.io>
2023-02-22 18:20:31 +0530

    added sanity backend

---
+++ client/sanity.js
@@ -0,0 +1,15 @@
+import { SanityClient } from "@sanity/client";
+import { createClient } from "@sanity/client";
+import { ImageUrlBuilder } from "@sanity/image-url/";
+
+export const client = createClient({
+    projectId: "lmtrfu4m",
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2021-10-21",
+})
+
+// const builder = ImageUrlBuilder(client)
+// export const urlFor = (source) => builder.image(source)
+// Run this to add exception for localhost 3333 CORS policy
+// Sanity cors add http://localhost:3333
--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { View, Text, Image, StyleSheet, StatusBar, TextInput, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {
@@ -10,15 +10,67 @@ import {
 } from "react-native-heroicons/outline";
 import Categories from '../components/Categories';
 import FeaturedRow from '../components/FeaturedRow';
+import { createClient } from '@sanity/client';
+
+const client = createClient({
+    projectId: 'lmtrfu4m',
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: "2022-01-12",
+});
 
 const HomeScreen = () => {
     const navigation = useNavigation();
+    const [featuredCategories, setFeaturedCategories] = useState(null)
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false,
         })
     })
+
+    // useEffect(() => {
+    //     const query = `*[_type == "featured"] {
+    //       ...,
+    //       restaurants[]->{
+    //         ...,
+    //         dishes[]->
+    //       }
+    //     }`;
+    //     client
+    //         .fetch(query)
+    //         .then((data) => {
+    //             setFeaturedCategories(data);
+    //         })
+    //         .catch((error) => console.log(error));
+    // }, []);
+
+    // useEffect(() => {
+    //     fetch('https://lmtrfu4m.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22featured%22%5D%20%7B%0A%20%20...%2C%0A%20%20restuarants%5B%5D-%3E%7B%0A%20%20%20%20...%2C%0A%20%20%20%20dishes%5B%5D-%3E%0A%20%20%7D%0A%7D')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setFeaturedCategories(data.result)
+    //             // console.log(data)
+    //             // const obj = JSON.parse(data)
+    //             // console.log(data)
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error:', error);
+    //         });
+    // }, [])
+    console.log(`
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    `)
+    // console.log(featuredCategories)
+
     return (
         <SafeAreaView className="bg-white pt-5">
             {/* <Text className='text-red-500'> */}
@@ -67,7 +119,20 @@ const HomeScreen = () => {
                 {/* Categories  */}
                 <Categories />
 
-                {/* Featured Rows */}
+                {/* Featured Rows
+                {featuredCategories?.map((category) => {
+                    console.log(category._id)
+                    console.log(category.name)
+                    console.log(category.short_description)
+                    // <FeaturedRow
+                    //     key={category._id}
+                    //     id={category._id}
+                    //     title={category.name}
+                    //     description={category.short_description}
+                    // />
+                })} */}
+
+
                 <FeaturedRow
                     id="123"
                     title="Featured"
